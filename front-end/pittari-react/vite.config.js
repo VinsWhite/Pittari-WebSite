11c92e711309d630eeb7a11b830fdf484d1c0f32
Vincenzo Saccone <143630974+VinsWhite@users.noreply.github.com>
2024-04-24 15:14:00 +0200

    Piccole aggiunte

--- front-end/pittari-react/vite.config.js
+++ front-end/pittari-react/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {
+      REACT_APP_SECRET_KEY: "valore_della_chiave_segreta"
+    },
+  },
 })
